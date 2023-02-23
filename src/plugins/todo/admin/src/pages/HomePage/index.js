f92ab129053511aefc9187ad32bfa8924f628ce9
miftajannat <miftajannat.dev@gmail.com>
2023-02-24 00:59:40 +0600

    build todo plugin second and third  components

--- src/plugins/todo/admin/src/pages/HomePage/index.js
+++ src/plugins/todo/admin/src/pages/HomePage/index.js
@@ -1,10 +1,7 @@
-/*
- *
- * HomePage
- *
- */
 
-import React, {memo} from 'react';
+
+import React, {memo, useState} from 'react';
+import { nanoid } from 'nanoid';
 // import PropTypes from 'prop-types';
 import pluginId from '../../pluginId';
 import {
@@ -12,8 +9,21 @@ import {
 BaseHeaderLayout,
 ContentLayout
  } from '@strapi/design-system';
+ import { EmptyStateLayout } from '@strapi/design-system';
+ import { Button } from '@strapi/design-system';
+  import { Plus } from '@strapi/icons';
+import { Illo } from '../../components/Illo';
+import TodoModal from '../../components/TodoModal';
+
 
 const HomePage = () => {
+  const [todoData, setTodoData] = React.useState([  ]);
+  const [showModal, setShowModal] = React.useState(false);
+
+  async function addTodo (data) {
+    setTodoData ([ ...todoData, {...data, id: nanoid(), isDone: false}]);
+  }``
+
   return (
     <Layout>
       <BaseHeaderLayout
@@ -22,10 +32,31 @@ const HomePage = () => {
        as = 'h2'
       />
       <ContentLayout>
-        <p >Happy Strapi coding</p>
+        {
+          todoData.length === 0 ? (
+          <EmptyStateLayout
+          icon= {<Illo />}
+          content = 'You don t have any todos yet'
+          action = {
+            <Button
+            onClick = {() => setShowModal(true)}
+            variant = 'secondary'
+            startIcon = {<Plus />}
+            >
+              Add a todo
+            </Button>
+          }
+          /> 
+          ): (
+             <p>count and Table</p>
+        )
+        }
       </ContentLayout>
+      {showModal && 
+      <TodoModal setShowModal={setShowModal} addTodo={addTodo} />
+      }
     </Layout>
   );
 };
 
-export default HomePage;
+export default memo(HomePage);
